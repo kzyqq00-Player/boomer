175ed091a7834370275bbcfa8fbb17e273c7da0a
kzyqq00-Player <1561737625@qq.com>
2024-12-03 23:52:13 +0800

    哎嘿

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { encrypt } from './website.vars.js';
-import * as readline from 'readline';
-const inputer = readline.createInterface({
+import { createInterface } from 'readline';
+const readline = createInterface({
     input: process.stdin,
     output: process.stdout,
 });
@@ -8,10 +8,10 @@ const inputer = readline.createInterface({
 const enabledEncriptToAnyChar = process.argv.includes('--encrypt-to-any-char') || process.argv.includes('-e2ac');
 const keepSpecialChars = (process.argv.includes('--keep-special-chars') || process.argv.includes('-ksc')) && enabledEncriptToAnyChar;
 
-inputer.prompt();
-inputer.on('line', (password) => {
+readline.prompt();
+readline.on('line', (password) => {
     console.log(encrypt(password, enabledEncriptToAnyChar, keepSpecialChars));
-    inputer.prompt();
+    readline.prompt();
 });
 process.on('SIGINT', () => {
     process.exit(0);
