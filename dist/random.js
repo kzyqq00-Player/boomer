5cc3500646b7c59b59b0150de47446e8cd577eff
kzyqq00-Player <1561737625@qq.com>
2024-12-04 05:55:02 +0800

    这样更好一点

--- dist/random.js
+++ dist/random.js
@@ -2,28 +2,27 @@ import { stdin, argv, exit } from 'node:process';
 import { emitKeypressEvents } from 'node:readline';
 import { fork } from 'node:child_process';
 const shouldOutputNowIndex = argv.includes('--output-now-index') || argv.includes('-oni');
-const childs = [];
-// @ts-ignore
-childs.sendAll = (message) => {
-    // @ts-ignore
-    childs.forEach((child) => {
-        child.send(message);
-    });
-};
-for (let i = 0; i < 8; i++) {
-    childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+const multipleProcesses = argv.includes('--cpu-dryer');
+const child = fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]);
+const childs = multipleProcesses ? [] : undefined;
+if (multipleProcesses) {
+    for (let i = 0; i < 7; i++) {
+        childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+    }
 }
 emitKeypressEvents(stdin);
 stdin.setRawMode(true);
-stdin.on('keypress', (key) => {
+stdin.on('keypress', multipleProcesses ? (key) => {
     switch (key) {
-        // @ts-ignore
+        case '\u0003': exit();
+    }
+} : (key) => {
+    switch (key) {
         case 'c':
-            childs.sendAll(false);
+            child.send(false);
             break;
-        // @ts-ignore
         case 'o':
-            childs.sendAll(true);
+            child.send(true);
             break;
         case '\u0003': exit();
     }
--- src/random.ts
+++ src/random.ts
@@ -1,28 +1,27 @@
 import { stdin, argv, exit } from 'node:process';
 import { emitKeypressEvents } from 'node:readline';
-import { fork, ChildProcess, Serializable } from 'node:child_process';
+import { fork, ChildProcess } from 'node:child_process';
 
 const shouldOutputNowIndex = argv.includes('--output-now-index') || argv.includes('-oni');
+const multipleProcesses = argv.includes('--cpu-dryer');
 
-const childs: ChildProcess[] = [];
-// @ts-ignore
-childs.sendAll = (message: boolean) => {
-    // @ts-ignore
-    childs.forEach((child: ChildProcess) => {
-        child.send(message);
-    });
-};
-for (let i = 0; i < 8; i++) {
-    childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+const child = fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]);
+const childs: ChildProcess[] = multipleProcesses ? [] : undefined;
+if (multipleProcesses) {
+    for (let i = 0; i < 7; i++) {
+        childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+    }
 }
 emitKeypressEvents(stdin);
 stdin.setRawMode(true);
-stdin.on('keypress', (key: string) => {
+stdin.on('keypress', multipleProcesses ? (key: string) => {
     switch (key) {
-        // @ts-ignore
-        case 'c': childs.sendAll(false); break;
-        // @ts-ignore
-        case 'o': childs.sendAll(true); break;
+        case '\u0003': exit();
+    }
+} : (key: string) => {
+    switch (key) {
+        case 'c': child.send(false); break;
+        case 'o': child.send(true); break;
         case '\u0003': exit();
     }
 });
