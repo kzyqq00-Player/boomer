6c161102a4af4ba5535ee0dbfefb180125dce296
kzyqq00-Player <1561737625@qq.com>
2024-12-03 16:04:12 +0800

    增加了一些神奇的东西

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import { encrypt } from './website.vars.js';
 import * as readline from 'readline';
 const inputer = readline.createInterface({
     input: process.stdin,
+    output: process.stdout,
 });
 
 const enabledEncriptToAnyChar = process.argv.includes('--encrypt-to-any-char') || process.argv.includes('-e2ac');
 const keepSpecialChars = (process.argv.includes('--keep-special-chars') || process.argv.includes('-ksc')) && enabledEncriptToAnyChar;
 
-(async function () {
-    while (1) {
-        await new Promise<void>((resolve) => {
-            inputer.question('', (password) => {
-                console.log(encrypt(password, enabledEncriptToAnyChar, keepSpecialChars));
-                resolve();
-            });
-        });
-    }
-})();
+inputer.prompt();
+inputer.on('line', (password) => {
+    console.log(encrypt(password, enabledEncriptToAnyChar, keepSpecialChars));
+    inputer.prompt();
+});
+process.on('SIGINT', () => {
+    process.exit(0);
+});
