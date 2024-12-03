f7defb738692684e2c5d89268ce493f99ea861c4
kzyqq00-Player <1561737625@qq.com>
2024-12-04 05:35:12 +0800

    充分榨干CPU

--- dist/random.js
+++ dist/random.js
@@ -2,16 +2,28 @@ import { stdin, argv, exit } from 'node:process';
 import { emitKeypressEvents } from 'node:readline';
 import { fork } from 'node:child_process';
 const shouldOutputNowIndex = argv.includes('--output-now-index') || argv.includes('-oni');
-const child = fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]);
+const childs = [];
+// @ts-ignore
+childs.sendAll = (message) => {
+    // @ts-ignore
+    childs.forEach((child) => {
+        child.send(message);
+    });
+};
+for (let i = 0; i < 8; i++) {
+    childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+}
 emitKeypressEvents(stdin);
 stdin.setRawMode(true);
 stdin.on('keypress', (key) => {
     switch (key) {
+        // @ts-ignore
         case 'c':
-            child.send(false);
+            childs[9].sendAll(false);
             break;
+        // @ts-ignore
         case 'o':
-            child.send(true);
+            childs.sendAll(true);
             break;
         case '\u0003': exit();
     }
--- src/random.ts
+++ src/random.ts
@@ -1,16 +1,28 @@
 import { stdin, argv, exit } from 'node:process';
 import { emitKeypressEvents } from 'node:readline';
-import { fork } from 'node:child_process';
+import { fork, ChildProcess, Serializable } from 'node:child_process';
 
 const shouldOutputNowIndex = argv.includes('--output-now-index') || argv.includes('-oni');
-const child = fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]);
 
+const childs: ChildProcess[] = [];
+// @ts-ignore
+childs.sendAll = (message: boolean) => {
+    // @ts-ignore
+    childs.forEach((child: ChildProcess) => {
+        child.send(message);
+    });
+};
+for (let i = 0; i < 8; i++) {
+    childs.push(fork((/\\dist/.test(argv[1]) || /\/dist/.test(argv[1])) ? 'dist/random_child_process.js' : 'random_child_process.js', [shouldOutputNowIndex.toString()]));
+}
 emitKeypressEvents(stdin);
 stdin.setRawMode(true);
 stdin.on('keypress', (key: string) => {
     switch (key) {
-        case 'c': child.send(false); break;
-        case 'o': child.send(true); break;
+        // @ts-ignore
+        case 'c': childs[9].sendAll(false); break;
+        // @ts-ignore
+        case 'o': childs.sendAll(true); break;
         case '\u0003': exit();
     }
 });
